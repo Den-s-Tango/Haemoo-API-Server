1877022e5f9a41a7aa9b5ae64c275a14b0d87882
twalla26 <ssm4748@naver.com>
2025-09-16 22:28:32 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/haemoo/haemooapi/HaemooApiApplication.java
@@ -0,0 +1,13 @@
+package com.haemoo.haemooapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaemooApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaemooApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haemoo/haemooapi/HaemooApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haemoo.haemooapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaemooApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
