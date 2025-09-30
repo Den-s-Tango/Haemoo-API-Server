5f6655209074045c5fb47934dae16c37fe1d2070
twalla26 <ssm4748@naver.com>
2025-09-30 14:07:13 +0900

    refactor: image가 아닌 video controller로 변경

--- src/main/java/com/haemoo/haemooapi/presentation/AwsS3Controller.java
+++ src/main/java/com/haemoo/haemooapi/presentation/AwsS3Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/images")
+@RequestMapping("/videos")
 @RequiredArgsConstructor
 public class AwsS3Controller {
 
