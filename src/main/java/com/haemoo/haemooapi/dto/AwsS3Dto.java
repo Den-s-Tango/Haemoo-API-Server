3b3f732e7598ae02fcdfdb03241f3758fbb62eba
twalla26 <ssm4748@naver.com>
2025-10-01 19:20:28 +0900

    feat: AWS S3 요청 응답 DTO 구현

--- src/main/java/com/haemoo/haemooapi/dto/AwsS3Dto.java
+++ src/main/java/com/haemoo/haemooapi/dto/AwsS3Dto.java
@@ -1,14 +1,70 @@
 package com.haemoo.haemooapi.dto;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
-public class PresignedUrlDto {
+import java.util.List;
 
-    private final String url;
+public class AwsS3Dto {
 
-    public PresignedUrlDto(String url) {
-        this.url = url;
+    @Data
+    @NoArgsConstructor
+    public static class InitiateUploadRequest {
+        private String fileName;
     }
 
+    @Data
+    @AllArgsConstructor
+    public static class InitiateUploadResponse {
+        private String uploadId;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class CompleteUploadRequest {
+        private String fileName;
+        private String uploadId;
+        private List<PartEtag> parts;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class PartEtag {
+        private int partNumber;
+        private String etag;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class MultipartPresignedUrlsRequest {
+        private String fileName;
+        private String uploadId;
+        private int partCount;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class MultipartPresignedUrlsResponse {
+        private List<String> presignedUrls;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class SinglePresignedUrlRequest {
+        private String fileName;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class SinglePresignedUrlResponse {
+        private String url;
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class AbortUploadRequest {
+        private String fileName;
+        private String uploadId;
+    }
 }
