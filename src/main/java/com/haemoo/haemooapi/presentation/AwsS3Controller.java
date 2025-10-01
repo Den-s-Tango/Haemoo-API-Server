4858ebc2a3875b56a2d0fa50cefac73eb4a9ddd7
twalla26 <ssm4748@naver.com>
2025-10-01 19:21:01 +0900

    feat: multipart upload 컨트롤러 구현

--- src/main/java/com/haemoo/haemooapi/presentation/AwsS3Controller.java
+++ src/main/java/com/haemoo/haemooapi/presentation/AwsS3Controller.java
@@ -1,36 +1,69 @@
 package com.haemoo.haemooapi.presentation;
 
 import com.haemoo.haemooapi.application.AwsS3Service;
-import com.haemoo.haemooapi.dto.PresignedUrlResponse;
+import com.haemoo.haemooapi.dto.AwsS3Dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/videos")
+@RequestMapping("api/videos")
 @RequiredArgsConstructor
 public class AwsS3Controller {
 
     private final AwsS3Service awsS3Service;
 
-    @GetMapping("/presigned-url/get")
-    public ResponseEntity<PresignedUrlResponse> getGetPresignedUrl(@RequestParam String fileName) {
-        String presignedUrl = awsS3Service.generateGetPresignedUrl(fileName);
-
-        PresignedUrlResponse response = new PresignedUrlResponse(presignedUrl);
-
-        return ResponseEntity.ok(response);
+    @PostMapping("upload/multipart/initiate")
+    public ResponseEntity<InitiateUploadResponse> initiateMultipartUpload(@RequestBody InitiateUploadRequest request) {
+        String uploadId = awsS3Service.initiateMultipartUpload(request.getFileName());
+        return ResponseEntity.ok(new InitiateUploadResponse(uploadId));
     }
 
-    @GetMapping("/presigned-url/put")
-    public ResponseEntity<PresignedUrlResponse> getPutPresignedUrl(@RequestParam String fileName) {
-        String presignedUrl = awsS3Service.generatePutPresignedUrl(fileName);
+    @PostMapping("upload/multipart/presigned-urls")
+    public ResponseEntity<MultipartPresignedUrlsResponse> getMultipartPresignedUrls(@RequestBody MultipartPresignedUrlsRequest request) {
 
-        PresignedUrlResponse response = new PresignedUrlResponse(presignedUrl);
+        String fileName = request.getFileName();
+        String uploadId = request.getUploadId();
+        int partCount = request.getPartCount();
 
-        return ResponseEntity.ok(response);
+        List<String> presignedUrls = awsS3Service.generatePresignedUrls(fileName, uploadId, partCount);
+
+        return ResponseEntity.ok(new MultipartPresignedUrlsResponse(presignedUrls));
+    }
+
+    @PostMapping("upload/multipart/complete")
+    public ResponseEntity<Void> completeMultipartUpload(@RequestBody CompleteUploadRequest request) {
+
+        String fileName = request.getFileName();
+        String uploadId = request.getUploadId();
+        List<PartEtag> parts = request.getParts();
+
+        awsS3Service.completeMultipartUpload(fileName, uploadId, parts);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("upload/multipart/abort")
+    public ResponseEntity<Void> abortMultipartUpload(@RequestBody AbortUploadRequest request) {
+
+        String fileName = request.getFileName();
+        String uploadId = request.getUploadId();
+
+        awsS3Service.abortMultipartUpload(uploadId, fileName);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("upload/single/presigned-url")
+    public ResponseEntity<SinglePresignedUrlResponse> getSinglePresignedUrl(@RequestBody SinglePresignedUrlRequest request) {
+
+        String fileName = request.getFileName();
+
+        String presignedUrl = awsS3Service.generateSinglePresignedUrl(fileName);
+
+        return ResponseEntity.ok(new SinglePresignedUrlResponse(presignedUrl));
     }
 }
